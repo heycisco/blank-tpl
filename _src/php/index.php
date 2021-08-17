525d4f42d579128c0f8a8573385390e22a887b79
heycisco <mail@starchenkov.pro>
2021-08-17 10:40:36 +0300

    +

--- _src/php/index.php
+++ _src/php/index.php
@@ -1,8 +1,21 @@
 <?php get_header(); ?>
 
-<div class="main-block wrapper">
-<h3>test</h3>
+<?php
+		// циклы вывода записей
+		// если записи найдены
+		if ( have_posts() ){
+			while ( have_posts() ){
+				the_post();
 
-</div>
+				echo '<h3><a href="'. get_permalink() .'">'. get_the_title() .'</a></h3>';
+
+				echo get_the_excerpt();
+			}
+		}
+		// елси записей не найдено
+		else{
+			echo ' <p>Записей нет...</p>';
+		}
+?>
 
 <?php get_footer(); ?>

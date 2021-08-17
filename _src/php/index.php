c178010f06036dfcb897a3e2a50524e1294115c7
heycisco <mail@starchenkov.pro>
2021-08-17 10:27:00 +0300

    Update index.php

--- _src/php/index.php
+++ _src/php/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <div class="main-block wrapper">
-<?php get_template_part( 'parts/homepage' );?>
 <h3>test</h3>
+<?php get_template_part( 'parts/homepage' );?>
 </div>
 
 <?php get_footer(); ?>

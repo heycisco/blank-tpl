be28eb71f5031c92b0169e0af1859ff3434b56b6
heycisco <mail@starchenkov.pro>
2021-08-17 10:21:10 +0300

    Update index.php

--- _src/php/index.php
+++ _src/php/index.php
@@ -1,19 +1,7 @@
 <?php get_header(); ?>
 
 <div class="main-block wrapper">
-<?php if ( is_front_page() ) {
-	get_template_part( 'parts/homepage' );
-}
-if ( is_category() ) {
-	get_template_part( 'parts/category' );
-}
-if (get_post_type() === 'post') {
-	get_template_part( 'parts/page' );
-}
-if (get_post_type() === 'page') {
-	get_template_part( 'parts/single-post' );
-}
-?>
+<?php get_template_part( 'parts/homepage' );?>
 </div>
 
 <?php get_footer(); ?>

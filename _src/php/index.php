564e6b878f970fe22d3f39ecfb2e8b1f8598a33e
heycisco <mail@starchenkov.pro>
2021-08-17 10:18:59 +0300

    -

--- _src/php/index.php
+++ _src/php/index.php
@@ -3,19 +3,15 @@
 <div class="main-block wrapper">
 <?php if ( is_front_page() ) {
 	get_template_part( 'parts/homepage' );
-	echo('homepage');
 }
 if ( is_category() ) {
 	get_template_part( 'parts/category' );
-	echo('category');
 }
 if (get_post_type() === 'post') {
 	get_template_part( 'parts/page' );
-	echo('page');
 }
 if (get_post_type() === 'page') {
 	get_template_part( 'parts/single-post' );
-	echo('single-post');
 }
 ?>
 </div>

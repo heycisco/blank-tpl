1c60142f03cc458f83de64961a6ae0c0309b3ccf
heycisco <mail@starchenkov.pro>
2021-08-18 14:46:26 +0300

    +

--- wp_theme/proj/category.php
+++ wp_theme/proj/category.php
@@ -4,10 +4,13 @@
 <h1 class="category__header__title"><?php single_term_title(); ?></h1>
 <div class="category__header__meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
 </header>
+
 <section class="category__posts content">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php endwhile; endif; ?>
+</section>
+
 <?php get_template_part( 'nav', 'below' ); ?>
 </main>
 <?php get_sidebar(); ?>

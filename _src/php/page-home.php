c12aadd0b6f9b8ce009e9fc45afb46f2cbede5dd
heycisco <mail@starchenkov.pro>
2021-08-18 18:11:25 +0300

    2

--- _src/php/page-home.php
+++ _src/php/page-home.php
@@ -4,11 +4,15 @@ Template Name: Homepage
 */
 ?>
 <?php get_header(); ?>
-<main class="main-block__page page homepage">
+<main class="main-block__page page">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article>
 <section class="page__content">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
+</article>
 <?php endwhile; endif; ?>
 </main>
 <?php get_sidebar(); ?>
--- wp_theme/proj/page.php
+++ wp_theme/proj/page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <main class="main-block__page page">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<article>
 <header class="header">
 <h1 class="entry-title"><?php the_title(); ?></h1>
 </header>
@@ -9,6 +10,7 @@
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
 </section>
+</article>
 <?php endwhile; endif; ?>
 </main>
 <?php get_sidebar(); ?>

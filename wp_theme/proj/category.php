1b0852bc85ea681d46988fa1ccb7fb121447dbb3
heycisco <mail@starchenkov.pro>
2021-08-19 14:33:42 +0300

    +

--- wp_theme/proj/category.php
+++ wp_theme/proj/category.php
@@ -1,26 +1,31 @@
 <?php get_header(); ?>
+
 <?php
-
-// Миниатюры категорий
-// получаем ID термина на странице термина
+// Изображение категории
 $term_id = get_queried_object_id();
-
-// получим ID картинки из метаполя термина
 $image_id = get_term_meta( $term_id, '_thumbnail_id', 1 );
-
-// ссылка на полный размер картинки по ID вложения
 $image_url = wp_get_attachment_image_url( $image_id, 'full' );
-
 ?>
+
+<?php if ( has_post_thumbnail() ) : ?>
+	<div class="category__header__image">
+		<?php echo '<img src="'. $image_url .'">'; ?>
+	</div>
+<?php endif; ?>
 
 
 <main class="main-block__category category">
 <header class="category__header">
-<h1 class="category__header__title"><?php single_term_title(); ?></h1>
-<div class="category__header__image">
-<?php echo '<img src="'. $image_url .'" alt="" />'; ?>
+	<h1 class="category__header__title">
+		<?php single_term_title(); ?>
+	</h1>
+
+
+<div class="category__header__meta">
+
+
+	<?php if ( '' != the_archive_description() ) { echo . '<div class="description">' . esc_html( the_archive_description() ); . '</div>' . } ?>
 </div>
-<div class="category__header__meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
 </header>
 
 <section class="category__posts content">
